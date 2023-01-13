718d08fdd1f041208ae38095de21ac5374c43c94
wfsec <69239087+wfsecs@users.noreply.github.com>
2023-01-13 22:54:15 +0200

    Asyncio & Threading and another API
    Can't tell if this made it worse than what it already were

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from colorama import Fore, init
 import threading
 import requests
+import asyncio
 import time
 
 init()
@@ -8,11 +9,6 @@ init()
 # Variables
 bad_status_codes = [403, 400, 500, 502]
 proxies = []
-protocol = 'http'
-timeout = '10000'
-country = 'all'
-ssl = 'all'
-anonymity = 'all'
 
 # Colors
 F_RED = Fore.RED
@@ -32,7 +28,7 @@ def get_original_ip():
     return ip_info['ip']
 
 
-def check_proxy(proxy):
+async def check_proxy(proxy):
     used_proxy = {"http": proxy}
 
     with open('working_proxies.txt', 'a+') as f:
@@ -48,59 +44,41 @@ def check_proxy(proxy):
                     print(f'    [{ip_info.status_code}] {L_GREEN}[+]{F_GREEN} Working proxy found >{RESET} "{proxy}" ')
                     f.write(f'''{proxy}
 ''')
-                else:
-                    print(f'    [{ip_info.status_code}]{L_RED} Connected with IP{L_BLACK} {ip_info.json()["ip"]}{L_RED} [-]{F_RED} Invalid proxy >{RESET} "{proxy}"')
+                # else:
+                    # print(f'    [{ip_info.status_code}]{L_RED} Connected with IP{L_BLACK} {ip_info.json()["ip"]}{L_RED} [-]{F_RED} Invalid proxy >{RESET} "{proxy}"')
         except:
             pass
 
 
-def check_proxies():
+async def check_proxies():
     global proxies
     for proxy in proxies:
-        Thread = threading.Thread(target=check_proxy, args=(proxy,), daemon=True)  # Starts the scanning thread
-        time.sleep(0.05) # Pause for a while to prevent crashing
+        Thread = threading.Thread(target=asyncio.run, args=(check_proxy(proxy),))  # Starts the scanning thread
+        time.sleep(0.03) # Pause for a while to prevent crashing
         Thread.start()
 
 
-def get_last_updated():
-    information = requests.get('https://api.proxyscrape.com/v2/?request=proxyinfo').json()
-    return information['last_updated']
+async def get_proxies():
+    urls = ['http://api.proxyscrape.com/v2/?request=displayproxies&protocol=http', 'https://www.proxy-list.download/api/v1/get?type=http']
 
+    print(f'    [?] Scraping from {len(urls)} APIs')
+    for url in urls:
+        proxylist = requests.get(url).text
 
-def get_proxy_count():
-    information = requests.get('https://api.proxyscrape.com/v2/?request=proxyinfo').json()
-    return information['proxy_count']
-
-
-def get_proxies():
-    url = f'http://api.proxyscrape.com/v2/?request=displayproxies&protocol={protocol}&timeout={timeout}&country={country}&ssl={ssl}&anonymity={anonymity}'
-    proxylist = requests.get(url).text
-
-    for proxy in proxylist.splitlines():
-        proxies.append(proxy)
-        print(f'    {L_BLUE}[%]{F_BLUE} Added{RESET} {proxy}{F_BLUE} to proxies.')
+        for proxy in proxylist.splitlines():
+            proxies.append(proxy)
+            print(f'    {L_BLUE}[%]{F_BLUE} Added{RESET} {proxy}{F_BLUE} to proxies.')
     else:
+        print(proxies)
         print(f'''{L_GREEN}    [+]{F_GREEN} Done scraping{RESET} {len(proxies)}{F_GREEN} proxies.
-    
-    {L_BLUE}[%]{F_BLUE} Checking proxies...{RESET}''')
+        
+        {L_BLUE}[%]{F_BLUE} Checking proxies...{RESET}''')
 
-        check_proxies()
+        await check_proxies()
 
-
-# Tell about proxies
-print(f'''
-    There are {F_YELLOW}{get_proxy_count()}{RESET} proxies...
-    Proxies were updated{F_YELLOW} {get_last_updated()}{RESET}
-    
-    Protocol: {F_YELLOW}{protocol}{RESET}
-    Timeout: {F_YELLOW}{timeout}{RESET}
-    Country: {F_YELLOW}{country}{RESET}
-    SSL: {F_YELLOW}{ssl}{RESET}
-    Anonymity: {F_YELLOW}{anonymity}{RESET}
-''')
 
 # Gets your IP. Proxies make your ip different so this script compares your IP to the Proxy IP
 original_ip = get_original_ip()
 
 # Start
-get_proxies()
+asyncio.run(get_proxies())
